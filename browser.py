1dea80276f50c853f8d4f143cc4a82fa09d8152d
e <1181355852@qq.com>
2021-03-24 16:59:38 +0800

    PyQt5 Gui

--- browser.py
+++ browser.py
@@ -9,6 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtWebEngineWidgets
 
 
 class Ui_Form(object):
@@ -26,6 +27,3 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-
-
-from PyQt5 import QtWebEngineWidgets
--- runbrowser.py
+++ runbrowser.py
@@ -4,3 +4,15 @@
 # @工程名:PyQt5 学习
 # @用户：Administrator  1181355852@qq.com
 # @IDE名字:PyCharm
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+import browser
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = browser.Ui_Form()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
